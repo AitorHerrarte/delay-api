7a5d5c5949c199ac84492955746763b9eae1a561
Aitor Herrarte Alonso <aitor.herrarte2003@gmail.com>
2023-12-20 11:50:02 +0100

    added all function

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/sleep/:seconds', (req, res) => {
+  const { seconds } = req.params;
+  const sleepTime = parseInt(seconds, 10) * 1000; 
+
+  setTimeout(() => {
+    res.send(`HAS ESPERADO ${seconds} segundos!`);
+  }, sleepTime);
+});
+
+const PORT = 3005; 
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
